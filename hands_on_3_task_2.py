4739cf767c771556adde5012cb45ff4eb2624ff9
araohatkokate <araohatk@gmail.com>
2024-09-08 15:50:21 -0500

    fitted polynomial

--- hands_on_3_task_2.py
+++ hands_on_3_task_2.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 import matplotlib.pyplot as plt
 
 def f(n):
@@ -8,7 +9,7 @@ def f(n):
             x += 1
 
 # Measure execution time for various values of n
-ns = range(1, 1000, 50)
+ns = range(1, 5000, 500)
 times = []
 
 for n in ns:
@@ -17,8 +18,12 @@ for n in ns:
     end_time = time.time()
     times.append(end_time - start_time)
 
+coefficients = np.polyfit(ns, times, 2)
+polynomial = np.poly1d(coefficients)
+
 # Plot time vs n
 plt.plot(ns, times, label='Execution Time')
+plt.plot(ns, polynomial(ns), label='Fitted Polynomial (degree 2)')
 plt.xlabel('n')
 plt.ylabel('Time (seconds)')
 plt.title('Execution Time of f(n) vs n')
